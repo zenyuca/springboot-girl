00856f3147aef40cd350870803ce7402988b67ef
zenyuca <zenyuca@163.com>
2017-08-10 17:51:39 +0800

    使用Looger(slf4j.jar)来打印日志，它会记录当前线程、时间、方法等核心信息

--- src/main/java/club/zenyuca/aspect/HttpAspect.java
+++ src/main/java/club/zenyuca/aspect/HttpAspect.java
@@ -4,6 +4,8 @@ import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 /**
@@ -15,17 +17,19 @@ import org.springframework.stereotype.Component;
 @Component
 public class HttpAspect {
 
+	private static final Logger logger = LoggerFactory.getLogger(HttpAspect.class);
+
 	@Pointcut(value = "execution(public * club.zenyuca.controller.GirlController.*(..))")
 	public void log() {
 	}
 
 	@Before("log()")
 	public void doBefore() {
-		System.out.println("我来了");
+		logger.info("我来了");
 	}
 
 	@After("log()")
 	public void doAfter() {
-		System.out.println("我走了");
+		logger.info("我走了");
 	}
 }
